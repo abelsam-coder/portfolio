ddc1698d2f52b7b9b0a8295f0e9497acfe97d91c
abelsam-coder <abelsamuel841@gmail.com>
2026-05-10 03:40:52 -0700

    a

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rs0&crv(4&ffcbgsz!2b8pjl^04wj#vd!7t4901r^&)-+9%d1v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
