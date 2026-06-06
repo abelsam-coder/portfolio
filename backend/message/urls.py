f517e58dd9ee7ea968d887226d24a16a76bae37c
Abel Samuel <abelsamuel841@gmail.com>
2026-06-06 07:03:37 -0700

    Update urls.py

--- backend/message/urls.py
+++ backend/message/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MessageApi
+from .views import MessageApi, StartServer
 
 urlpatterns = [
-       path('message/',MessageApi.as_view())
+       path('message/',MessageApi.as_view()),
+       path('start/', StartServer.as_view())
 ]
