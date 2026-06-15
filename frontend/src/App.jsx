1b99b33980bfeb60c2d2a263b40e69272a3cb1c3
Abel Samuel <abelsamuel841@gmail.com>
2026-06-15 07:37:07 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ export default function App(){
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   )
